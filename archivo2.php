1a10e24f51f76408f894bed0e624865810713ab9
camilo Marrugo Barrios <marrugobarrioscamilo2005@gmail.com>
2024-06-25 15:08:46 -0500

    hola

--- archivo2.php
+++ archivo2.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>a</title>
 </head>
+
 <body>
+    <h2>holalaslsllas</h2>
+    <h2>holalaslsllas</h2>
+    <h2>holalaslsllas</h2>
+    <h2>holalaslsllas</h2>
+    <h2>holalaslsllas</h2>
+    <h2>holalaslsllas</h2>
+    <h2>holalaslsllas</h2>
     <h5>hola</h5>
 </body>
+
 </html>

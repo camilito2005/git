d8b68f34a4cc554da8a3b9f3d2113fc63e24e68c
camilo Marrugo Barrios <marrugobarrioscamilo2005@gmail.com>
2024-06-25 14:15:23 -0500

    cambiando

--- archivo2.php
+++ archivo2.php
@@ -1,24 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>a</title>
 </head>
+
 <body>
-    <div>
-        <h4>tabla</h4>
-        <table>
-            <thead>
-            <tr>
-                <th>Nombre</th>
-                <th>Apellido</th>
-            </tr>
-            </thead>
-            <tr>
-                
-            </tr>
-        </table>
-    </div>
+    <p>hola</p>
 </body>
+
 </html>
